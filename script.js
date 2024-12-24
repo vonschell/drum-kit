2bfc9f7d18b5549d84531ddf1e9d98d0a1867c56
Vonschell <vonschell02@gmail.com>
2024-12-24 19:44:07 +0900

    Created a new variable and added an event listener to listen to the end of the transition

--- script.js
+++ script.js
@@ -12,3 +12,5 @@ function removeTransition(e) {
     this.classList.remove('playing');
 }
 
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
